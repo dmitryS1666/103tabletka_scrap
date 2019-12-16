f570c11b88f0cd97e24fd1ebd5414cefdccd629e
Dmitry <dsuschinsky@gmail.com>
2019-12-17 00:19:31 +0300

    debug

--- scraper.rb
+++ scraper.rb
@@ -4,22 +4,31 @@
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
+require "csv"
 
 def scraper
   url = 'https://apteka.103.by/lekarstva-minsk/'
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
   tablets = Array.new
-  tablet_listings = parsed_page.css('div.abc-list')
-  byebug # точка останова
+  tablet_listings = parsed_page.css('div.abc-list').css('div.col')
   tablet_listings.each do |tablet_listing|
-    tablet = {
-      title: tablet_listing.css('').text,
-      url: tablet_listing.css('').attributes['href'].value
-    }
-    tablets << tablet
+    tablet_listing.css('ul.list').css('li').each do |listing|
+      # byebug # точка останова
+      tablet = {
+          title: listing.css('a')[0].text,
+          url: listing.css('a')[0].attributes['href'].value,
+          desc: listing.css('span.mnn-description')[0].text.include?('(~)') ? nil : listing.css('span.mnn-description')[0].text
+      }
+      tablets << tablet
+    end
   end
   puts 'tablets', tablets
+  CSV.open("file.csv", "wb") do |csv|
+    csv << %w(title url desc)
+    csv << tablets
+  end
+
   #byebug # точка останова
 end
 
